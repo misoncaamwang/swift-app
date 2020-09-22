f36a64a26ca49bc3ca5fb3dfeef5407745bd28bf
Mison <mison.caamwang@hotmail.com>
2020-09-23 01:47:45 +1000

    final

--- myApp/ViewController.swift
+++ myApp/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    // @IBAction is similar to @IBOutlet , but goes the other way: @IBOutlet is a way of connecting code to storyboard layouts, and @IBAction is a way of making storyboard layouts trigger code.
+    
+    var audioPlayer:AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func topLeftTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "bruh", withExtension: ".mp3")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func topRightTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "cut", withExtension: ".mp3")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        } catch {
+            print(error)
+        }
+    }
+    @IBAction func botLeftTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "dood", withExtension: ".mp3")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        } catch {
+            print(error)
+        }
+    }
+    @IBAction func botRightTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "who", withExtension: ".mp3")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        } catch {
+            print(error)
+        }
+    }
 }
 
